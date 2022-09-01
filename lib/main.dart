72cb5b186f5edfb564f4bc7a482564538e4789df
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2022-09-01 11:57:51 -0300

    Atualizado

--- lib/main.dart
+++ lib/main.dart
@@ -1,80 +1,16 @@
+import 'package:animacoescomplexas/home/home_screen.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Animacoes Intro',
+      title: 'Animations',
       debugShowCheckedModeBanner: false,
-      home: LoginApp(),
+      home: HomeScreen(),
     );
   }
-}
-
-class LoginApp extends StatefulWidget {
-
-  @override
-  State<LoginApp> createState() => _LoginApp();
-}
-
-class _LoginApp extends State<LoginApp> with SingleTickerProviderStateMixin {
-
-  late AnimationController controller;
-  late Animation<double> animation;
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    controller = AnimationController(
-      vsync: this,
-      duration: Duration(seconds: 2)
-    );
-
-    animation = Tween<double>(begin: 0, end: 300).animate(controller);
-    animation.addStatusListener((status) {
-      if(status == AnimationStatus.completed) {
-        controller.reverse();
-      } else if (status == AnimationStatus.dismissed) {
-        controller.forward();
-      }
-    });
-
-    controller.forward();
-  }
-
-  @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return AnimatedLogo(animation);
-  }
-}
-
-
-class AnimatedLogo extends AnimatedWidget {
-  final Animation<double> animation;
-
-  AnimatedLogo(Animation<double> this.animation) : super(listenable: animation);
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Center(
-      child: Container(
-        height: animation.value,
-        width: animation.value,
-        child: FlutterLogo(),
-      ),
-    );
-  }
-  
 }
